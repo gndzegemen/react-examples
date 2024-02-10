5e6c285c47e1c1922861832b445cba29e9422b46
Egemen Gunduz <86930714+gndzegemen@users.noreply.github.com>
2024-02-10 12:16:46 +0300

    create: init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+
+
+function App() {
+    return (
+        <div className="App">
+            <h3>{process.env.NODE_ENV}</h3>
+            {process.env.NODE_ENV === 'production' && (
+                <>
+                    <img src="/logo192.png" alt=""/>
+                    <img src={logo} alt=""/>
+                </>
+            )}
+
+        </div>
+    );
+}
+
+export default App;
