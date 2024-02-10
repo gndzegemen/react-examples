54e4eeb317823d223afb04f6b9da9f7247285569
Egemen Gunduz <86930714+gndzegemen@users.noreply.github.com>
2024-02-10 18:44:02 +0300

    feat: create env file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
+import {useEffect} from "react";
+import './/style.css'
 
 
 function App() {
+
+    useEffect(()=> {
+        if(process.env.NODE_ENV === "production"){
+            //do something
+        }
+    },[])
     return (
         <div className="App">
             <h3>{process.env.NODE_ENV}</h3>
+            <p>{process.env.REACT_APP_API_URL}</p>
             {process.env.NODE_ENV === 'production' && (
                 <>
                     <img src="/logo192.png" alt=""/>
                     <img src={logo} alt=""/>
+
                 </>
             )}
 
